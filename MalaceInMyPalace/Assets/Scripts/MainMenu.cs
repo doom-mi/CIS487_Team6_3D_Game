06a489ba6d167a89e55886edc2b76862d847ce09
Steven Price <sprice7287@gmail.com>
2023-12-03 10:54:36 -0500

    semi functionality setup
    Some reason had to comment out the sample game main menu? idk why
    
    Other than that set up basic functionality quit game, and prepped the play and settings for navigating to next scene

--- MalaceInMyPalace/Assets/Scripts/MainMenu.cs
+++ MalaceInMyPalace/Assets/Scripts/MainMenu.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public void Play()
     {
-        
+        // call first level or level selection scene
+        UnityEngine.Debug.Log("Navigating to Level Selector Scene");
+
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Settings()
     {
-        
+        // call to go to setting scene
+        UnityEngine.Debug.Log("Navigating to Settings Scene");
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        UnityEngine.Debug.Log("Application Quitting");
     }
 }
